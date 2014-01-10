a1ae367ec94734eed51e1d0cdd2b1b60afde7042
Harry He <zhedahht@hotmail.com>
2014-01-10 16:37:39 +0800

    Initial check-in for FunctionParameters

---
+++ 02_FunctionParameters/FunctionParameters.js
@@ -0,0 +1,92 @@
+$(document).ready(function () {
+    (function() {
+        function greeting(name, greetingWord) {
+            if (greetingWord === undefined) {
+                greetingWord = "Hi";
+            }
+            
+            console.log(greetingWord + ", " + name);
+        }
+        
+        greeting("Harry", "Hello"); // Hello, Harry 
+        greeting("Peter"); // Hi, Peter
+    })();
+
+    (function() {
+        function add() {
+            var sum = 0;
+            for(var i = 0; i < arguments.length; ++i) {
+                sum += arguments[i];
+            }
+            
+            return sum;
+        }
+        
+        console.log(add(1, 2, 3)); // 6
+        console.log(add(1, 2, 3, 4)) // 10
+    })();
+    
+    (function() {
+        var threeToPowerOfTen = (function (exponent) {
+            if (exponent === 1) {
+                return 3;
+            }
+            return 3 * arguments.callee(exponent - 1);
+        })(10);
+        
+        console.log(threeToPowerOfTen); // 59049 
+    })();
+    
+    (function() {
+        function power(base, exponent) {
+            if (exponent == 1) {
+                return base
+            }
+        
+            return base * power(base, exponent - 1);
+        }
+        
+        console.log(power(2, 3)); // 8
+    })();
+    
+    (function() {
+        function power(base, exponent) {
+            if (exponent == 1) {
+                return base
+            }
+        
+            return base * power(base, exponent - 1);
+        }
+        
+        var realPower = power;
+        
+        power = function(name) {
+            console.log("Grant power to", name);
+        }
+        
+        console.log(realPower(2, 3));
+        
+        // Output:
+        // Grant power to 2
+        // NaN
+    })();
+    
+    (function() {
+        function power(base, exponent) {
+            if (exponent == 1) {
+                return base
+            }
+        
+            return base * arguments.callee(base, exponent - 1);
+        }
+        
+        var realPower = power;
+        
+        power = function(name) {
+            console.log("Grant power to", name);
+        }
+        
+        console.log(realPower(2, 3)); // 8
+    })();
+});
+
---
+++ 02_FunctionParameters/README.md
@@ -0,0 +1,6 @@
+FunctionParameters
+============
+
+JavaScript is Different - Function Parameters
+
+Here is the sample code focusing on function parameters in JavaScript.
